4860305d572e4c3a2fe10d3893a941fb537729d9
Purity Maina <puritymaina001@gmail.com>
2017-10-10 11:46:52 +0300

    Update models.py

--- models.py
+++ models.py
@@ -8,9 +8,8 @@ from model_utils.models import TimeStampedModel
 
 class Schedule(TimeStampedModel):
     """
-    A model to hold any relation from a subject to other subjects
-    """
-    session = models.ForeignKey(Session)
+    A model to hold any relation from a user
+    "" "
     date_and_time = models.DateTimeField(blank=True, null=True)
     series = models.CharField(blank=True, null=True, max_length=200)
     date_year = models.CharField(blank=True, null=True, max_length=200)
